ac1e4101c258ea22bbad1c6c2b5d8d1b0d9f037c
Lucas Nunes <lucas.jlnunes@gmail.com>
2025-02-05 14:11:37 -0300

    add mongoose

--- backend/app.js
+++ backend/app.js
@@ -5,6 +5,8 @@ const cors = require("cors");
 const cookieParser = require("cookie-parser");
 const mongoSanitize = require("express-mongo-sanitize");
 
+const path = require('path') // Para acessar file system
+
 const PORT = process.env.PORT || 4000;
 const app = express();
 
@@ -18,5 +20,15 @@ app.use(cors({
   origin:[`https://localhost:${PORT}`], // Aceita esse servidor
   credentials: true // Enviara cookies de sessão APENAS para o endereço de origem
 }))
+
+app.use(express.static(path.join(__dirname, 'public'))); // Para carregar arquivos da pasta public
+
+
+
+if(process.env.NODE_ENV === 'development') app.use(morgan('dev')); // Para informações adicionar no modo dev
+
+app.use(express.json({limit: "10kb"})) // Para proteger o servidor
+
+app.use(mongoSanitize()) //Para evitar ataques de query selector
 
 module.exports = app;
--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,17 @@
+const mongoose = require("mongoose")
 const app = require("./app");
 require("dotenv").config();
-const PORT = process.env.PORT || 4000;
+
+mongoose.connect(process.env.DB).then(() => {
+    console.log(`DB Connection successfull`)
+}).catch((err) => {
+    console.log(err)
+})
+
+const PORT = process.env.PORT || 3000;
+
+
+
 const server = app.listen(PORT, () => {
     console.log(`App running on port ${PORT}`)
 })
