5f6306953f6185d654e882fdb54d10f561a7198d
Lucas Nunes <lucas.jlnunes@gmail.com>
2025-02-11 11:44:44 -0300

    initial header

---
+++ ecommerce/src/components/Header.tsx
@@ -0,0 +1,13 @@
+
+const Header = () => {
+  return (
+    <header className="w-full fixed bg-white z-10 top-0 h-[60px] md:h-[90px] flex justify-between pr-2 items-center max-w-screen-2xl mx-auto">
+      <div className="flex justify-center items-center">
+
+        <h1 className="text-2xl md:text-4xl font-extrabold pl-2">Online Store</h1>
+      </div>
+    </header>
+  )
+}
+
+export default Header
